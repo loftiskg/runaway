77a2ab2b2ab23d3ac764701c56c0f22d1c890ef4
Kevin Loftis <loftiskg@gmail.com>
2020-06-26 09:37:25 -0700

    update cli

--- play.py
+++ play.py
@@ -12,10 +12,10 @@ from src.game import Game
 def run(agent_type, model_path, verbose):
     game = Game(randomize_start_pos=False)
 
-    if agent_type == "td":
+    if agent_type == "q":
         my_agent = TD_Agent()
         my_agent.load_model(model_path)
-    
+
     if agent_type == "random":
         my_agent = Random_Agent(STATE_SIZE, ACTION_SIZE)
 
@@ -33,15 +33,15 @@ def run(agent_type, model_path, verbose):
         for event in events:
             if event.type == pygame.QUIT:
                 sys.exit()
-        
-        if agent_type == 'human':
+
+        if agent_type == "human":
             move = move if my_agent.act(events) is None else my_agent.act(events)
         else:
             move = my_agent.act(state)
         state, reward, done, _ = game.step(move)
 
         if verbose:
-            print(state, reward, done,move)
+            print(state, reward, done, move)
         if done:
             break
 
@@ -53,16 +53,16 @@ def run(agent_type, model_path, verbose):
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("--agent", choices=["human", "td", 'random'], default='human')
+    parser.add_argument("--agent", choices=["human", "q", "random"], default="human")
     parser.add_argument("--agent_model_path", default=None)
     parser.add_argument("--verbose", "-v", action="store_true")
-    
+
     args = parser.parse_args()
     agent_type = args.agent
     agent_model_path = args.agent_model_path
     verbose = args.verbose
 
-    if agent_model_path is None and args == "dqn":
+    if agent_model_path is None and args == "td":
         raise ValueError("Must specify the path to model if playing in agent mode")
 
     run(agent_type, agent_model_path, verbose)
